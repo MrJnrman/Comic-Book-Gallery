c9248fa3b17fd4077bf57eaad353857e7cc9015b
Jamel Reid <jnoelreid@gmail.com>
2016-07-24 17:43:38 -0800

    Updated the Comic Book Detail View Layout

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -15,7 +15,7 @@ namespace ComicBookGallery.Controllers
 
             var comicBook = new ComicBook()
             {
-                SeriesTitle = "The Amazing Spiderman",
+                SeriesTitle = "The Amazing Spider Man",
                 IssueNumber = 700,
                 DescriptionHtml = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
@@ -26,7 +26,7 @@ namespace ComicBookGallery.Controllers
                     new Artist() { Name = "Edgar Delgado", Role = "Colors" },
                     new Artist() { Name = "Chris Eliopolous", Role = "Letters" }
                 }
-        };
+             };
 
             //if (DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
             //{
